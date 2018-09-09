734552e781cf435b3d675241f27a82a1fa3d6f7a
Sunho Kim <sunho207@gmail.com>
2018-09-09 00:43:06 -0400

    minor UI fix

--- src/containers/login/styles/Register.js
+++ src/containers/login/styles/Register.js
@@ -53,7 +53,7 @@ export default StyleSheet.create({
     fontWeight: '700'
   },
   verified: {
-    top: -25,
+    top: -30,
     marginBottom: -20,
     right: Dimensions.get('window').width * -0.4 + 10
   },
