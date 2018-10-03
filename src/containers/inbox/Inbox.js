8f14380cd53633ad8ecb6b8416a1ffe897e3bf16
akhan36 <akhan36@github.com>
2018-10-03 09:58:33 -0400

    No

--- src/containers/inbox/Inbox.js
+++ src/containers/inbox/Inbox.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Text, View, Button } from 'react-native';
 import { connect } from 'react-redux';
-import ChatKit from "@pusher/chatkit";
-import styles from './styles/Inbox'
+import Chatkit from "@pusher/chatkit";
+import styles from './styles/Inbox';
+import { GiftedChat } from "react-native-gifted-chat";
 
 
-<<<<<<< HEAD
-=======
+
+
 const CHATKIT_TOKEN_PROVIDER_ENDPOINT = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/d3ac1342-3bc8-4b05-b682-4cacbfea946e/token";
 const CHATKIT_INSTANCE_LOCATOR = "v1:us1:d3ac1342-3bc8-4b05-b682-4cacbfea946e";
-const CHATKIT_ROOM_ID = "17318184";
-const CHATKIT_USER_NAME = "Anas Khan"; // Let's chat as "Dave" for this tutorial
+const CHATKIT_ROOM_ID = 17318184;
+const CHATKIT_USER_NAME = "akhan95@gatech.edu";
 
->>>>>>> master
 class Inbox extends React.Component {
 
   constructor(props) {
@@ -23,20 +23,17 @@ class Inbox extends React.Component {
   }
 
 componentDidMount() {
-    // This will create a `tokenProvider` object. This object will be later used to make a Chatkit Manager instance.
     const tokenProvider = new Chatkit.TokenProvider({
       url: CHATKIT_TOKEN_PROVIDER_ENDPOINT
     });
 
-    // This will instantiate a `chatManager` object. This object can be used to subscribe to any number of rooms and users and corresponding messages.
-    // For the purpose of this example we will use single room-user pair.
+
     const chatManager = new Chatkit.ChatManager({
       instanceLocator: CHATKIT_INSTANCE_LOCATOR,
       userId: CHATKIT_USER_NAME,
       tokenProvider: tokenProvider
     });
 
-    // In order to subscribe to the messages this user is receiving in this room, we need to `connect()` the `chatManager` and have a hook on `onNewMessage`. There are several other hooks that you can use for various scenarios. A comprehensive list can be found [here](https://docs.pusher.com/chatkit/reference/javascript#connection-hooks).
     chatManager.connect().then(currentUser => {
       this.currentUser = currentUser;
       this.currentUser.subscribeToRoom({
